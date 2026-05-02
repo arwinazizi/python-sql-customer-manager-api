b28eb9cae7b1c1f6707e3a63aac115a964e3672b
arwin azizi <arwin2002@hotmail.se>
2026-05-02 11:05:03 +0200

    feat: add initial in-memory customer

---
+++ app/main.py
@@ -0,0 +1,13 @@
+customers = []
+
+customer = {
+    "id": 1,
+    "name": "Ali Hassan",
+    "email": "ali@example.com",
+    "phone": "0701234567",
+    "company": "Ali Consulting"
+}
+
+customers.append(customer)
+
+print(customers)
