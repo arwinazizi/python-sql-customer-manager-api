966a017281fd10a2445448b57156884c35fbf427
arwin azizi <arwin2002@hotmail.se>
2026-05-02 11:12:51 +0200

    feat: add multiple in-memory customers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 customers = []
 
-customer = {
+customer1 = {
     "id": 1,
     "name": "Ali Hassan",
     "email": "ali@example.com",
@@ -8,6 +8,24 @@ customer = {
     "company": "Ali Consulting"
 }
 
-customers.append(customer)
+customer2 = {
+    "id": 2,
+    "name": "Sara Ahmed",
+    "email": "sara@example.com",
+    "phone": "0707654321",
+    "company": "Sara Design"
+}
+
+customer3 = {
+    "id": 3,
+    "name": "Lina Josef",
+    "email": "lina@example.com",
+    "phone": "0707454321",
+    "company": "Lina AI"
+}
+
+customers.append(customer1)
+customers.append(customer2)
+customers.append(customer3)
 
 print(customers)
